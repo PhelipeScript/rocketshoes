401ce82c71e21642e1aa95c8e76da0bd5944926c
Phelipe <phelipescript@gmail.com>
2022-11-28 11:59:53 -0300

    creating dynamic card in JS

--- main.js
+++ main.js
@@ -96,3 +96,36 @@ carousel.addEventListener('touchmove', dragging)
 carousel.addEventListener('mouseup', dragStop)
 carousel.addEventListener('mouseleave', dragStop)
 carousel.addEventListener('touchend', dragStop)
+
+let id = 0
+function createCard(image, prodName, price, promoPrice) {
+    id++
+    return `
+            <div id=${id} class="card">
+                <div class="image">
+                <img src="${image}" alt="">
+                </div>
+                <div class="name-price">
+                <h3>${prodName}</h3>
+                <div class="price-box">
+                    <span class="price">R$ ${price}</span>
+                    <span class="promo-price">R$ ${promoPrice}</span>
+                </div>
+                </div>
+                <div class="buy-more">
+                    <button class="buy">Comprar</button>
+                    <button class="see-more"><i class="fa-solid fa-eye"></i></button>
+                </div>
+            </div>
+    `
+}
+
+const products = document.querySelector('.products')
+
+products.innerHTML = 
+    createCard('assets/img-1.jpeg', 'Mizuno Wave Prophecy 8', '699.99', '429.99') + createCard('assets/img-2.png', 'Mizuno Wave Prophecy 6', '699.99', '429.99') 
+    + createCard('assets/img-3.jpeg', 'Nike Shox 12 Molas', '499.99', '319.99') 
+    + createCard('assets/img-4.jpeg', 'Mizuno Wave Prophecy 9', '699.99', '429.99') 
+    + createCard('assets/img-5.jpeg', 'Mizuno Wave Prophecy 8', '699.99', '429.99') 
+    + createCard('assets/img-6.png', 'Mizuno Wave Prophecy 7', '699.99', '429.99') 
+    + createCard('assets/img-7.jpeg', 'Mizuno Wave Prophecy 7', '699.99', '429.99') 
